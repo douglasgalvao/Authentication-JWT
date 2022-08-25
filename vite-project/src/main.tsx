8426064e1ca225fade783bf9b3ffe4520129a510
Douglas Machado <douglas-mad@live.com>
2022-08-24 23:58:07 -0300

    Main component

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import Login from './pages/home/Login'
+import NavTest from './pages/home/NavTest'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { ContextProvider } from './context/AuthContext'
+import Home from './pages/home/Home'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
   <BrowserRouter>
+  <ContextProvider>
     <Routes>
-      <Route path='/' element={<Login />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='/about' element={<NavTest />} />
     </Routes>
+    </ContextProvider>
   </BrowserRouter>
   </React.StrictMode>
 )
